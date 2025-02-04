bc0c1c162882918d49f2133ec184b96e139e3243
tukuf <bitam11-061@suza.ac.tz>
2025-02-04 17:13:52 +0300

    Update settings.py

--- RealEstate/settings.py
+++ RealEstate/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-06#e(!6k&(d39&qkb-m*u)u_zo)xa@4ryb+9eg66xcmirrxfwm
 DEBUG = True
 AUTH_USER_MODEL = 'Land.User'
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['https://backend-gqfp.onrender.com ', '127.0.0.1', 'localhost']
 
 # URL ya static files
